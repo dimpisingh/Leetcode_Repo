62fbd07e04eea98eae44613654cb11e37c3f9364
Dimpi singh <54979541+dimpisingh@users.noreply.github.com>
2024-07-26 13:08:10 +0530

    Time: 11 ms (81.5%), Space: 20.6 MB (38.35%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int xr1=0, xr2=0;
-        for(int i=0;i<=nums.size();i++){
-            xr1^=i;
+        int xr1 = 0, xr2 = 0;
+        for (int i = 0; i <= nums.size(); i++) {
+            xr1 ^= i;
         }
-        for(int i=0;i<nums.size();i++){
-            xr2 = xr2^nums[i];
+        for (int i = 0; i < nums.size(); i++) {
+            xr2 = xr2 ^ nums[i];
         }
-        return xr1^xr2;
-
+        return xr1 ^ xr2;
     }
 };
