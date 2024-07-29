25e1ec125146a2cb027c55de11dc88fe0b7ced13
Dimpi singh <54979541+dimpisingh@users.noreply.github.com>
2024-07-29 17:08:16 +0530

    Time: 272 ms (45.81%), Space: 122.1 MB (14.84%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,28 +1,68 @@
 class MedianFinder {
 public:
     MedianFinder() {}
+    // using quickselect
+    // void addNum(int num) { nums.push_back(num); }
+    // double findMedian() {
+    //     int n = nums.size();
+    //     if (n % 2 == 1)
+    //         return quickSelect(0, n - 1, n / 2);
+    //     else
+    //         return (quickSelect(0, n - 1, n / 2 - 1) +
+    //                 quickSelect(0, n - 1, n / 2)) /
+    //                2.0;
+    // }
+    // int quickSelect(int left, int right, int k) {
+    //     if (left == right) {
+    //         return nums[left];
+    //     }
+    //     int pivotIndex = left + rand() % (right - left + 1);
+    //     pivotIndex = partition(left, right, pivotIndex);
+    //     if (k == pivotIndex) {
+    //         return nums[k];
+    //     } else if (k < pivotIndex) {
+    //         return quickSelect(left, pivotIndex - 1, k);
+    //     } else {
+    //         return quickSelect(pivotIndex + 1, right, k);
+    //     }
+    // }
+    // int partition(int left, int right, int pivotIndex) {
+    //     int pivotValue = nums[pivotIndex];
+    //     swap(nums[pivotIndex], nums[right]);
+    //     int storeIndex = left;
 
-    void addNum(int num) {
-        if (lower.empty()) {
-            lower.push(num);
-            return;
-        }
-        if (lower.size() > higher.size()) {
-            if (lower.top() > num) {
-                higher.push(lower.top());
-                lower.pop();
-                lower.push(num);
-            } else
-                higher.push(num);
-        } else {
-            if (num > higher.top()) {
-                lower.push(higher.top());
-                higher.pop();
-                higher.push(num);
-            } else
-                lower.push(num);
-        }
-    }
+    //     for (int i = left; i < right; ++i) {
+    //         if (nums[i] < pivotValue) {
+    //             swap(nums[i], nums[storeIndex]);
+    //             ++storeIndex;
+    //         }
+    //     }
+    //     swap(nums[storeIndex], nums[right]);
+    //     return storeIndex;
+    // }
+    // vector<int> nums;
+    // using min amd max heap
+     void addNum(int num) {
+         if (lower.empty()) {
+             lower.push(num);
+             return;
+         }
+         if (lower.size() > higher.size()) {
+             if (lower.top() > num) {
+                 higher.push(lower.top());
+                 lower.pop();
+                 lower.push(num);
+             } else
+                 higher.push(num);
+         } else {
+             if (num > higher.top()) {
+                 lower.push(higher.top());
+                 higher.pop();
+                 higher.push(num);
+             } else
+                 lower.push(num);
+         }
+     }
 
     double findMedian() {
         double result = 0.0;
