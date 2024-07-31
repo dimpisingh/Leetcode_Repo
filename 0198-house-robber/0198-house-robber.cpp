2bb0624c6aab83474a51f8d3f588fc5041da1176
Dimpi singh <54979541+dimpisingh@users.noreply.github.com>
2024-07-31 22:57:35 +0530

    Time: 4 ms (31.42%), Space: 9.2 MB (54.09%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int r1 = 0, r2 = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            int tmp = max(nums[i] + r1, r2);
+            r1 = r2;
+            r2 = tmp;
+        }
+        return r2;
+    }
+};
