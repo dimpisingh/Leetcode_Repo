f041ef6dcac35954db76d5749d3789af4f661645
Dimpi singh <54979541+dimpisingh@users.noreply.github.com>
2024-08-12 11:49:30 +0530

    Time: 940 ms (15.92%), Space: 74.8 MB (26.98%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -5,27 +5,21 @@ public:
         if (n == 0) {
             return 0;
         }
-        unordered_set<int> st(nums.begin(),nums.end());
-        //int cnt = 1;
+        unordered_set<int> st(nums.begin(), nums.end());
+
         int longest = 1;
-        int longestStreak=0;
+
         for (int num : nums) {
-        if (st.find(num - 1) == st.end()) { // Start of a new sequence
-            longestStreak =0 ;
-            while (st.find(num + longestStreak) != st.end()) {
-                //cnt += 1;
-                longestStreak += 1;
-                
+            if (st.find(num - 1) == st.end()) { // Start of a new sequence
+                int cnt = 1;
+                int x = num;
+                while (st.find(x + 1) != st.end()) {
+                    x = x + 1;
+                    cnt += 1;
+                }
+                longest = max(longest, cnt);
             }
-            longest = max(longest, longestStreak);        
-        }
-        
         }
         return longest;
     }
-
-    
-
-    
-    
 };
