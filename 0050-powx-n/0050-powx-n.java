552397de3d9769395d1e9344f8e0c13ed02efc7b
Dimpi singh <54979541+dimpisingh@users.noreply.github.com>
2024-08-29 23:58:41 +0530

    Time: 0 ms (100%), Space: 41.8 MB (76.82%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,24 +1,43 @@
 class Solution {
     public double myPow(double x, int n) {
-    if (n == 0) return 1;
-    if (n == 1) return x;
+        if (n == 0)
+            return 1;
+        if (n == 1)
+            return x;
+        //T.C O(logn) S.C O(1)
+        // long N = n; // Use long to avoid overflow when n = -2147483648
+        // if (N < 0) {
+        // x = 1 / x;
+        // N = -N;
+        // }
+        double result = 1.0;
+        // while (N > 0) {
+        // if ((N % 2) == 1) { // If N is odd
+        // result *= x;
+        // }
+        // x *= x; // Square the base
+        // N /= 2; // Divide N by 2
+        // }
+        // return result;
 
-    long N = n; // Use long to avoid overflow when n = -2147483648
-    if (N < 0) {
-        x = 1 / x;
-        N = -N;
-    }
-
-    double result = 1.0;
-    while (N > 0) {
-        if ((N % 2) == 1) { // If N is odd
-            result *= x;
+        // T.C O(log n) S.C O(1)
+        long N = n;
+        if (N < 0) {
+            x = 1 / x; // Invert x if n is negative
+            N = -N; // Make n positive
         }
-        x *= x; // Square the base
-        N /= 2; // Divide N by 2
+        while (N > 0) {
+            if (N % 2 == 1) {
+                result *= x;
+                N--;
+            } else {
+                x *= x;
+                N = N / 2;
+            }
+        }
+        // if (n < 0)
+        //     result = (double) (1.0) / (double) (result);
+        return result;
     }
-
-    return result;
-}
 
 }
