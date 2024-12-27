2beb49e420016244eb5a1794ce23d95ffa2cbab7
Dimpi singh <54979541+dimpisingh@users.noreply.github.com>
2024-12-27 13:58:25 +0530

    Time: 0 ms (100%), Space: 8.7 MB (8.57%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     int climbStairs(int n) {
+        int* memo = new int[n + 1]();
+        int result = helper(n, memo);
+        delete[] memo; // Free the dynamically allocated memory
+        return result;
+    }
+
+    int helper(int n, int memo[]) {
         if (n == 1 || n == 2)
             return n;
-        int res;
-        int first = 1, second = 2;
-        for (int i = 2; i < n; i++) {
-            res = first + second;
-            first = second;
-            second = res;
-        }
-        return res;
+        if (memo[n] != 0)
+            return memo[n];
+        memo[n] = helper(n - 1, memo) + helper(n - 2, memo);
+        return memo[n];
     }
 };
