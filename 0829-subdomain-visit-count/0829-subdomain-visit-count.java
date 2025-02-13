a1be4725062c45b7e812c3d803d716f82aed53f5
Dimpi singh <54979541+dimpisingh@users.noreply.github.com>
2025-02-13 11:05:19 +0530

    Time: 16 ms (79.5%), Space: 45.7 MB (29.26%) - LeetHub

---
+++ 0829-subdomain-visit-count/0829-subdomain-visit-count.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<String> subdomainVisits(String[] cpdomains) {
+        List<String> result = new ArrayList<>();
+        Map<String, Integer> map = new HashMap<>(); // key: subdomain, value: frequency
+        StringBuilder resultStringBuilder = new StringBuilder();
+
+        for (String cpdomain : cpdomains) {
+            int indexSpace = cpdomain.indexOf(' ');
+            int numClicks = Integer.parseInt(cpdomain.substring(0, indexSpace));
+            String domain = cpdomain.substring(indexSpace + 1);
+            resultStringBuilder.setLength(0);
+            resultStringBuilder.append(domain);
+            while (true) {
+                map.put(resultStringBuilder.toString(), map.getOrDefault(resultStringBuilder.toString(), 0) + numClicks);
+                int dotPosition = resultStringBuilder.indexOf(".");
+                if (dotPosition == -1)
+                    break;
+                resultStringBuilder.delete(0, dotPosition + 1);
+            }
+        }
+
+        for (String domain : map.keySet())
+            result.add(map.get(domain) + " " + domain);
+
+        return result;
+    }
+}
