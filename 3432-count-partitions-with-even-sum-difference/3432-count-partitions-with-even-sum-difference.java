5a8f0a730449a93135dcc0ca0ad3ec52eb53481a
Dimpi singh <54979541+dimpisingh@users.noreply.github.com>
2025-12-05 20:33:23 +0530

    Time: 0 ms (100%), Space: 43.5 MB (31.42%) - LeetHub

--- 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.java
+++ 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.java
@@ -1,6 +1,8 @@
 class Solution {
     public int countPartitions(int[] A) {
-         int total = Arrays.stream(A).sum();
+        int total = 0;
+        for (int x : A)
+            total += x;
         return (total & 1) == 0 ? A.length - 1 : 0;
     }
 }
