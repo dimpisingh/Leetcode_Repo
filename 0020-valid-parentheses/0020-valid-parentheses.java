1298ac79d0f87647f3f52c710c92bfdf91bd1e3c
Dimpi singh <54979541+dimpisingh@users.noreply.github.com>
2025-12-20 00:53:13 +0530

    Time: 3 ms (82.26%), Space: 43.4 MB (17.8%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,25 +1,24 @@
 class Solution {
-    public boolean isValid(String p) {
-        Stack<Character> st = new Stack<>();
-        char c = ' ';
-        char[] s = p.toCharArray();
-        for (int i = 0; i < s.length; i++) {
+    public boolean isValid(String s) {
+    Stack<Character> st = new Stack<>();
 
-            if (s[i] == '{' || s[i] == '(' || s[i] == '[') {
-                st.push(s[i]);
-            }
+    for (char c : s.toCharArray()) {
+        if (c == '{' || c == '[' || c == '(') {
+            st.push(c);
+        } else {
+            if (st.isEmpty()) return false;
 
-            else {
-                if (st.isEmpty())
-                    return false;
-                c = st.peek();
-                if ((c == '{' && s[i] == '}') || (c == '(' && s[i] == ')') || (c == '[' && s[i] == ']')) {
-                    st.pop();
-                } else {
-                    return false; // mismatched pair
-                }
+            char t = st.peek();
+            if ((t == '{' && c == '}') ||
+                (t == '[' && c == ']') ||
+                (t == '(' && c == ')')) {
+                st.pop();
+            } else {
+                return false;
             }
         }
-        return st.isEmpty();
+    }
+
+    return st.isEmpty();
     }
 }
