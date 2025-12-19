79b8a89eed4727a8bfba57d4387767420fef0f31
Dimpi singh <54979541+dimpisingh@users.noreply.github.com>
2025-12-20 01:34:20 +0530

    Time: 45 ms (5.23%), Space: 48.3 MB (6.23%) - LeetHub

--- 0929-unique-email-addresses/0929-unique-email-addresses.java
+++ 0929-unique-email-addresses/0929-unique-email-addresses.java
@@ -2,22 +2,22 @@ class Solution {
     public int numUniqueEmails(String[] emails) {
         HashSet<String> st = new HashSet<>();
 
-        for(String email:emails){
+        for (String email : emails) {
             int atPos = email.indexOf("@");
-            String localNm = email.substring(0,atPos);
+            String localNm = email.substring(0, atPos);
             String domainNm = email.substring(atPos);
-            if(localNm.contains(".")){
-                localNm = localNm.replaceAll("\\.","");
-            }
-            if(localNm.contains("+")){
+
+            localNm = localNm.replaceAll("\\.", "");
+
+            if (localNm.contains("+")) {
                 int pl = localNm.indexOf("+");
-                localNm = localNm.substring(0,pl);
+                localNm = localNm.substring(0, pl);
             }
-            st.add(localNm+domainNm);
-            System.out.println(localNm+domainNm);
+            st.add(localNm + domainNm);
+            System.out.println(localNm + domainNm);
 
         }
         return st.size();
-        
+
     }
 }
