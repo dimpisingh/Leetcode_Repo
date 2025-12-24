db59613e0c55d1c14dd3ec1bb84b74912fa2e77f
Dimpi singh <54979541+dimpisingh@users.noreply.github.com>
2025-12-24 23:04:58 +0530

    Time: 4 ms (82.79%), Space: 44.9 MB (5.34%) - LeetHub

---
+++ 3074-apple-redistribution-into-boxes/3074-apple-redistribution-into-boxes.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minimumBoxes(int[] apple, int[] capacity) {
+      int total = 0;
+        for (int a : apple) {
+            total += a;
+        }
+        
+        // Sorts in ascending order
+        Arrays.sort(capacity);
+        
+        int cnt = 0;
+        // Iterate from the end to pick largest boxes first
+        for (int i = capacity.length - 1; i >= 0; i--) {
+            total -= capacity[i];
+            cnt++;
+            if (total <= 0) {
+                return cnt;
+                }
+        }
+        return cnt;  
+    }
+}
