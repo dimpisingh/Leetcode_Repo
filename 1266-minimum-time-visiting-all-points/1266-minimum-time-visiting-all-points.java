76f2ce303995d663f0ecfc17c2f19cce163a5c7a
Dimpi singh <54979541+dimpisingh@users.noreply.github.com>
2026-01-12 23:38:44 +0530

    Time: 0 ms (100%), Space: 44.7 MB (87.06%) - LeetHub

---
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.java
@@ -0,0 +1,14 @@
+class Solution {
+    static int maxNorm(int[] P, int[] Q){
+        return Math.max(Math.abs(P[0]-Q[0]), Math.abs(P[1]-Q[1]));
+    }
+    static public int minTimeToVisitAllPoints(int[][] points) {
+        int ans=0;
+        int[] prev=points[0];
+        for (int[] P: points){
+            ans+=maxNorm(P, prev);
+            prev=P;
+        }
+        return ans;
+    }
+}
