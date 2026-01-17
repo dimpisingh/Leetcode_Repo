3ed4fffbf91be84be08883646e07a59cf88f0284
Dimpi singh <54979541+dimpisingh@users.noreply.github.com>
2026-01-17 18:52:13 +0530

    Time: 25 ms (100%), Space: 47.7 MB (66.67%) - LeetHub

---
+++ 3047-find-the-largest-area-of-square-inside-two-rectangles/3047-find-the-largest-area-of-square-inside-two-rectangles.java
@@ -0,0 +1,20 @@
+class Solution {
+    public long largestSquareArea(int[][] bottomLeft, int[][] topRight) {
+        int n = bottomLeft.length;
+        long max_side = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) { 
+                int width = Math.min(topRight[i][0], topRight[j][0]) - 
+                Math.max(bottomLeft[i][0], bottomLeft[j][0]);
+                int height = 
+                    Math.min(topRight[i][1], topRight[j][1]) - 
+                    Math.max(bottomLeft[i][1], bottomLeft[j][1]);
+                int side = Math.min(width, height);
+                max_side = Math.max(max_side, side);
+                }
+        }
+
+        return max_side * max_side;
+
+    }
+}
