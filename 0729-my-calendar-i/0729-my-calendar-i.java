a1344b78ccfae74ed0104d41067d404f819ddf87
Dimpi singh <54979541+dimpisingh@users.noreply.github.com>
2026-01-23 11:05:11 +0530

    Time: 24 ms (64.55%), Space: 47.4 MB (55.07%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.java
+++ 0729-my-calendar-i/0729-my-calendar-i.java
@@ -1,20 +1,27 @@
 class MyCalendar {
-    private List<int[]> bookings;
+    private TreeMap<Integer, Integer> bookings;
     public MyCalendar() {
-        bookings = new ArrayList<>();
+        bookings = new TreeMap<>();
     }
     
     public boolean book(int startTime, int endTime) {
         //if(!bookings.isEmpty()){
-        for(int[] event : bookings){
-            int s = event[0];
-            int e = event[1];
-            if(startTime <e && s< endTime ){
-                return false;
-            }
-        }
-        //}
-        bookings.add(new int[]{startTime, endTime});
+        // for(int[] event : bookings){
+        //     int s = event[0];
+        //     int e = event[1];
+        //     if(startTime <e && s< endTime ){
+        //         return false;
+        //     }
+        // }
+        // //}
+        // bookings.add(new int[]{startTime, endTime});
+        // return true;
+        Integer prev = bookings.floorKey(startTime);
+        if(prev!=null && bookings.get(prev)> startTime)
+        return false;
+        Integer next = bookings.ceilingKey(startTime);
+        if (next!=null && endTime > next) return false;
+        bookings.put(startTime, endTime);
         return true;
         
     }
