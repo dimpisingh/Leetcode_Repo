a63e5228ff210aa7577f91f7e6b73a40543ef702
Dimpi singh <54979541+dimpisingh@users.noreply.github.com>
2026-01-26 10:31:07 +0530

    Time: 60 ms (5.14%), Space: 75 MB (5.03%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.java
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.java
@@ -1,31 +1,35 @@
 class Solution {
     public List<List<Integer>> minimumAbsDifference(int[] arr) {
-       int OFFSET = 1_000_000;
-       int RANGE = 2_000_001;
-       boolean[] present = new boolean[RANGE];
-       for(int num: arr){
-        present[num+OFFSET] = true;
-       }
-       List<List<Integer>> result = new ArrayList<>();
-       int prev = -1;
-       int minDiff = Integer.MAX_VALUE;
-       for(int i=0;i<RANGE; i++){
-        if(present[i]){
-            int curr = i-OFFSET;
-            if(prev !=-1){
-                int diff = curr-prev;
-                if(diff < minDiff){
+        //    int OFFSET = 1_000_000;
+        //    int RANGE = 2_000_001;
+        //    boolean[] present = new boolean[RANGE];
+        //    for(int num: arr){
+        //     present[num+OFFSET] = true;
+        //    }
+        TreeSet<Integer> st = new TreeSet<>();
+        for (int n : arr) {
+            st.add(n);
+        }
+        List<List<Integer>> result = new ArrayList<>();
+        Integer prev = null;
+        int minDiff = Integer.MAX_VALUE;
+        //for(int i=0;i<RANGE; i++){
+        //if(present[i]){
+        //int curr = i-OFFSET;
+        for (int curr : st) {
+            if (prev != null) {
+                int diff = curr - prev;
+                if (diff < minDiff) {
                     minDiff = diff;
                     result.clear();
-                    result.add(Arrays.asList(prev,curr));
-                }
-                else if(diff==minDiff){
-                    result.add(Arrays.asList(prev,curr));
+                    result.add(Arrays.asList(prev, curr));
+                } else if (diff == minDiff) {
+                    result.add(Arrays.asList(prev, curr));
                 }
             }
             prev = curr;
         }
-       }
-       return result;
+
+        return result;
     }
 }
