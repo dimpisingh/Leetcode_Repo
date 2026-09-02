930ff1ee0d4aa575e67dd5dbefe762b876b47dc3
Dimpi singh <54979541+dimpisingh@users.noreply.github.com>
2026-09-02 17:04:23 +0530

    Time: 0 ms (100%), Space: 30.3 MB (67.8%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -1,17 +1,7 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        int i=0,j=0;
-        bool ans = true;
-        for(i;i<nums1.size(); i++){
-            if(nums1[i]%2==0){
-                continue;
-            }
-            else {
-                nums1[i]-nums1[j];
-                j++;
-            }
-        }
+        
         return true;
     }
 };
