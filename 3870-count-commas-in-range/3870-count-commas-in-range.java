105e8bd85a557c1cf962cf2f90e3ff36b33e74ec
Dimpi singh <54979541+dimpisingh@users.noreply.github.com>
2026-09-08 11:03:29 +0530

    Time: 1 ms (83.65%), Space: 42.5 MB (57.08%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -3,14 +3,7 @@ class Solution {
 
         if (n < 1000)
             return 0;
-        // count++;
-        int tmp = n;
-        // while(n>1000){
-        //     //count++;
-        //     r=n%1000;
-        //     count+=r;
-        //     n=n%1000;
-        // }
-        return tmp - 999;
+        
+        return n - 999;
     }
 }
